a3dd2607579efc349c90e537f77bb7e1e6a7edaf
DevExpressExampleBot <fakeEmail>
2022-12-16 22:07:37 +0400

    Source auto update [skip ci]

--- VB/DxAccordionGettingStarted/DataModel.vb
+++ VB/DxAccordionGettingStarted/DataModel.vb
@@ -1,133 +1,45 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 
 Namespace DxAccordionGettingStarted
-	Public Class Employee
-		Public Property ID() As Integer
-		Public Property Name() As String
-		Public Property Position() As String
-		Public Property Department() As String
-		Public Overrides Function ToString() As String
-			Return Name
-		End Function
-	End Class
 
-	Public Module Stuff
-		Public Function GetStuff() As List(Of Employee)
-'INSTANT VB NOTE: The variable stuff was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim stuff_Conflict As New List(Of Employee)()
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 1,
-				.Name = "Gregory S. Price",
-				.Department = "Management",
-				.Position = "President"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 2,
-				.Name = "Irma R. Marshall",
-				.Department = "Marketing",
-				.Position = "Vice President"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 3,
-				.Name = "John C. Powell",
-				.Department = "Operations",
-				.Position = "Vice President"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 4,
-				.Name = "Christian P. Laclair",
-				.Department = "Production",
-				.Position = "Vice President"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 5,
-				.Name = "Karen J. Kelly",
-				.Department = "Finance",
-				.Position = "Vice President"
-			})
+    Public Class Employee
 
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 6,
-				.Name = "Brian C. Cowling",
-				.Department = "Marketing",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 7,
-				.Name = "Thomas C. Dawson",
-				.Department = "Marketing",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 8,
-				.Name = "Angel M. Wilson",
-				.Department = "Marketing",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 9,
-				.Name = "Bryan R. Henderson",
-				.Department = "Marketing",
-				.Position = "Manager"
-			})
+        Public Property ID As Integer
 
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 10,
-				.Name = "Harold S. Brandes",
-				.Department = "Operations",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 11,
-				.Name = "Michael S. Blevins",
-				.Department = "Operations",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 12,
-				.Name = "Jan K. Sisk",
-				.Department = "Operations",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 13,
-				.Name = "Sidney L. Holder",
-				.Department = "Operations",
-				.Position = "Manager"
-			})
+        Public Property Name As String
 
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 14,
-				.Name = "James L. Kelsey",
-				.Department = "Production",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 15,
-				.Name = "Howard M. Carpenter",
-				.Department = "Production",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 16,
-				.Name = "Jennifer T. Tapia",
-				.Department = "Production",
-				.Position = "Manager"
-			})
+        Public Property Position As String
 
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 17,
-				.Name = "Judith P. Underhill",
-				.Department = "Finance",
-				.Position = "Manager"
-			})
-			stuff_Conflict.Add(New Employee() With {
-				.ID = 18,
-				.Name = "Russell E. Belton",
-				.Department = "Finance",
-				.Position = "Manager"
-			})
-			Return stuff_Conflict
-		End Function
-	End Module
+        Public Property Department As String
+
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+    End Class
+
+    Public Module Stuff
+
+        Public Function GetStuff() As List(Of Employee)
+            Dim stuff As List(Of Employee) = New List(Of Employee)()
+            stuff.Add(New Employee() With {.ID = 1, .Name = "Gregory S. Price", .Department = "Management", .Position = "President"})
+            stuff.Add(New Employee() With {.ID = 2, .Name = "Irma R. Marshall", .Department = "Marketing", .Position = "Vice President"})
+            stuff.Add(New Employee() With {.ID = 3, .Name = "John C. Powell", .Department = "Operations", .Position = "Vice President"})
+            stuff.Add(New Employee() With {.ID = 4, .Name = "Christian P. Laclair", .Department = "Production", .Position = "Vice President"})
+            stuff.Add(New Employee() With {.ID = 5, .Name = "Karen J. Kelly", .Department = "Finance", .Position = "Vice President"})
+            stuff.Add(New Employee() With {.ID = 6, .Name = "Brian C. Cowling", .Department = "Marketing", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 7, .Name = "Thomas C. Dawson", .Department = "Marketing", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 8, .Name = "Angel M. Wilson", .Department = "Marketing", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 9, .Name = "Bryan R. Henderson", .Department = "Marketing", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 10, .Name = "Harold S. Brandes", .Department = "Operations", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 11, .Name = "Michael S. Blevins", .Department = "Operations", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 12, .Name = "Jan K. Sisk", .Department = "Operations", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 13, .Name = "Sidney L. Holder", .Department = "Operations", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 14, .Name = "James L. Kelsey", .Department = "Production", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 15, .Name = "Howard M. Carpenter", .Department = "Production", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 16, .Name = "Jennifer T. Tapia", .Department = "Production", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 17, .Name = "Judith P. Underhill", .Department = "Finance", .Position = "Manager"})
+            stuff.Add(New Employee() With {.ID = 18, .Name = "Russell E. Belton", .Department = "Finance", .Position = "Manager"})
+            Return stuff
+        End Function
+    End Module
 End Namespace
--- VB/DxAccordionGettingStarted/ViewModel.vb
+++ VB/DxAccordionGettingStarted/ViewModel.vb
@@ -1,27 +1,32 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.Linq
 
 Namespace DxAccordionGettingStarted
-	Public Class MainWindowViewModel
-		Public Sub New()
-'INSTANT VB NOTE: The variable employeeDepartments was renamed since Visual Basic does not handle local variables named the same as class members well:
-			Dim employeeDepartments_Conflict = Stuff.GetStuff().GroupBy(Function(x) x.Department).Select(Function(x) New EmployeeDepartment(x.Key, x.ToArray()))
-			EmployeeDepartments = New ObservableCollection(Of EmployeeDepartment)(employeeDepartments_Conflict.ToArray())
-		End Sub
-		Public Property EmployeeDepartments() As ObservableCollection(Of EmployeeDepartment)
-	End Class
 
-	Public Class EmployeeDepartment
-		Public Property Name() As String
-		Public Property Employees() As ObservableCollection(Of Employee)
+    Public Class MainWindowViewModel
 
-		Public Sub New(ByVal name As String, ByVal employees As IEnumerable(Of Employee))
-			Me.Name = name
-			Me.Employees = New ObservableCollection(Of Employee)(employees)
-		End Sub
-		Public Overrides Function ToString() As String
-			Return Name
-		End Function
-	End Class
+        Public Sub New()
+            Dim employeeDepartments = GetStuff().GroupBy(Function(x) x.Department).[Select](Function(x) New EmployeeDepartment(x.Key, x.ToArray()))
+            Me.EmployeeDepartments = New ObservableCollection(Of EmployeeDepartment)(employeeDepartments.ToArray())
+        End Sub
+
+        Public Property EmployeeDepartments As ObservableCollection(Of EmployeeDepartment)
+    End Class
+
+    Public Class EmployeeDepartment
+
+        Public Property Name As String
+
+        Public Property Employees As ObservableCollection(Of Employee)
+
+        Public Sub New(ByVal name As String, ByVal employees As IEnumerable(Of Employee))
+            Me.Name = name
+            Me.Employees = New ObservableCollection(Of Employee)(employees)
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return Name
+        End Function
+    End Class
 End Namespace
